2e725b72365fde35974fd8541cacc2ca2897c16b
Anthony Lockett <alockett@rccl.com>
2024-06-11 07:11:16 -0400

    added proj initial commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Anthony Lockett on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
